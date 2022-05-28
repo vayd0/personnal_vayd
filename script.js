f902850e475366caa072359330d61dff198392ab
vayd0 <kiiroxyz@gmail.com>
2022-05-28 22:49:42 +0200

    v2

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-$(document).ready(function() {
-  $(".navbar-burger").click(function() {
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
-  });
+let counterContainer = document.querySelector(".counter");
+let visitCount = localStorage.getItem("views");
+localStorage.setItem("views", 1);
+visitCount = Number(visitCount) + 1;
+localStorage.setItem("views", visitCount);
+counterContainer.innerHTML = visitCount + " visites";
+
+let loader = document.getElementById("loader");
+window.addEventListener("load", function () {
+  loader.style.display = "none";
 });
