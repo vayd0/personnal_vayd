c7c256c749fdd3781394ab665025c84f50321fb3
vayd0 <kiiroxyz@gmail.com>
2022-07-12 18:53:40 +0200

    vayd

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-let counterContainer = document.querySelector(".counter");
-let visitCount = localStorage.getItem("views");
-localStorage.setItem("views", 1);
-visitCount = Number(visitCount) + 1;
-localStorage.setItem("views", visitCount);
-counterContainer.innerHTML = visitCount + " visites";
-
-let loader = document.getElementById("loader");
-window.addEventListener("load", function () {
-  setTimeout(() => {
-    loader.style.display = "none";
-  }, 1500);
+$(document).ready(function() {
+  $(".navbar-burger").click(function() {
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+  });
 });
